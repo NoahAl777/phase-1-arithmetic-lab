45a2dab1014511e754c8d97402d7b08ca01b0768
Noah <crashlive112@gmail.com>
2021-11-04 23:51:40 -0400

    commit tree dec4dc7d4a270973c5d855ef22c76001862504fa

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.ceil(Math.random());
+
+const num3 = 25;
+const num4 = 7;
+let mod = num3 % num4;
+
+const max = Math.max(2, 4, 3, 20);
